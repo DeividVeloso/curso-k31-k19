83fde561df6e944ed59f1a1aceb148f4ab7ee316
SoulReaperNZ <veloso.deivid@gmail.com>
2016-01-22 10:43:42 -0200

    Novos Projetos

--- Organizacao/Sistema/Testes/Teste.cs
+++ Organizacao/Sistema/Testes/Teste.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PrimeiroProjeto;
 
 namespace Organizacao.Sistema.Testes
 {
@@ -12,8 +13,10 @@ namespace Organizacao.Sistema.Testes
 		static void Main(string[] args)
 		{
 			Conta c = new Conta();
+			Cliente cli = new Cliente();
+			cli.ClienteID = 100;
 			c.Deposita(100);
-			System.Console.WriteLine("Saldo :" + c.Saldo);
+			System.Console.WriteLine("Saldo : " + c.Saldo +" ClienteID: " + cli.ClienteID);
 		}
 	}
 }
--- PrimeiroProjeto/Cliente.cs
+++ PrimeiroProjeto/Cliente.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace PrimeiroProjeto
 {
-	class NameSpace
+	public class Cliente
 	{
+		public int ClienteID { get; set; }
 	}
 }
