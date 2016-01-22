96fb8f98c26297e679ee5a618014104d961fbc69
SoulReaperNZ <veloso.deivid@gmail.com>
2016-01-22 14:36:27 -0200

    Novo método Hash

--- Colecoes/HashCode.cs
+++ Colecoes/HashCode.cs
@@ -12,9 +12,21 @@ namespace Colecoes
 		{
 			Conta conta1 = new Conta() { Numero = 20, Saldo = 100 };
 			Conta conta2 = new Conta() { Numero = 20, Saldo = 100 };
-		
-			Console.WriteLine(conta1.Equals(conta2));
 
+			Console.WriteLine("HasCode da conta 1: {0}", conta1.GetHashCode());
+			Console.WriteLine("HasCode da conta 2: {0}", conta2.GetHashCode());
+
+
+			Console.WriteLine(conta1.Equals("Ola")); // false
+			Console.WriteLine(conta1.Equals(null)); // false
+			Console.WriteLine(conta1.Equals(conta1)); // true
+			Console.WriteLine(conta1.Equals(conta2)); // true
+
+			var contas = new HashSet<Conta>();
+			contas.Add(conta1);
+			contas.Add(conta2);
+
+			Console.WriteLine(contas.Count); //1
 
 		}
 	}
